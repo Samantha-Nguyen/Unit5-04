2dea7309c7b968174617476ac43828c1d84482d1
SamanthaNguyen1 <>
2021-05-09 14:12:13 +0000

    Comments added

--- script.js
+++ script.js
@@ -8,23 +8,28 @@ document.getElementById('show').addEventListener('click', show)
 
 document.getElementById('background-color').addEventListener('click', background)
 
+// This makes the first button change to the second image added in the images file
 function image () {
   document.getElementById('img1').src = './images/kaguya-2.png'
 }
 
+// This makes the second button change the text and color of the paragraph "O Kawaii"
 function text () {
   document.getElementById('text').innerHTML = 'How cute'
   document.getElementById('text').style.color = '#fff'
 }
 
+// This button is used to hide the text
 function hide () {
   document.getElementById('text').style.display = 'none'
 }
 
+// This button makes the text reappear
 function show () {
   document.getElementById('text').style.display = 'block'
 }
 
+// This button changes the backgroun
 function background () {
-  document.body.style.backgroundColor = '	#0d1426'
+  document.body.style.backgroundColor = '#0d1426'
 }
